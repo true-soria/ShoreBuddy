0d17ea3c1f1325fa697289de69635e9ab03323a9
true4evr <true4evr@gmail.com>
2020-03-11 09:56:35 -0700

    Solunar Class and JSONSolunarParser are updated. They now include fields and fetching respectively for Major and minor times.

--- app/src/main/java/com/example/shorebuddy/data/solunar/JSONSolunarParser.java
+++ app/src/main/java/com/example/shorebuddy/data/solunar/JSONSolunarParser.java
@@ -25,6 +25,23 @@ class JSONSolunarParser {
         solunar.moonUnder = root.getString("moonUnder");
         solunar.moonUnderDouble = root.getDouble("moonUnderDec");
 
+        solunar.minor1Start = root.getString("minor1Start");
+        solunar.minor1StartDouble = root.getDouble("minor1StartDec");
+        solunar.minor1Stop = root.getString("minor1Stop");
+        solunar.minor1StopDouble = root.getDouble("minor1StopDec");
+        solunar.minor2Start = root.getString("minor2Start");
+        solunar.minor2StartDouble = root.getDouble("minor2StartDec");
+        solunar.minor2Stop = root.getString("minor2Stop");
+        solunar.minor2StopDouble = root.getDouble("minor2StopDec");
+        solunar.major1Start = root.getString("major1Start");
+        solunar.major1StartDouble = root.getDouble("major1StartDec");
+        solunar.major1Stop = root.getString("major1Stop");
+        solunar.major1StopDouble = root.getDouble("major1StopDec");
+        solunar.major2Start = root.getString("major2Start");
+        solunar.major2StartDouble = root.getDouble("major2StartDec");
+        solunar.major2Stop = root.getString("major2Stop");
+        solunar.major2StopDouble = root.getDouble("major2StopDec");
+
         solunar.sunRise = root.getString("sunRise");
         solunar.sunRiseDouble = root.getDouble("sunRiseDec");
         solunar.sunTransit = root.getString("sunTransit");
