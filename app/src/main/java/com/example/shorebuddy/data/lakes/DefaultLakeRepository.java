79901bfd8a6619ed31d00281eee0ddeceb0f0ec3
Mitch Labrenz <MitchSLabrenz@gmail.com>
2020-03-14 19:51:05 -0700

    Move filtering into SQL query and out of the LakeRepo
    This commit also fixes some layout constraints that were broken.

--- app/src/main/java/com/example/shorebuddy/data/lakes/DefaultLakeRepository.java
+++ app/src/main/java/com/example/shorebuddy/data/lakes/DefaultLakeRepository.java
@@ -3,18 +3,14 @@ package com.example.shorebuddy.data.lakes;
 import android.app.Application;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.example.shorebuddy.data.ShoreBuddyDatabase;
 import com.example.shorebuddy.utilities.SearchQuery;
 import java.util.List;
-import java.util.Objects;
-import java.util.Vector;
 
 public class DefaultLakeRepository implements LakeRepository {
 
     private LakeDao lakeDao;
-    private MutableLiveData<List<Lake>> filteredLakes = new MutableLiveData<>();
     private LiveData<List<Lake>> allLakes;
 
     public DefaultLakeRepository(Application application){
@@ -35,13 +31,6 @@ public class DefaultLakeRepository implements LakeRepository {
 
     @Override
     public LiveData<List<Lake>> getFilteredLakes(SearchQuery query) {
-        Vector<Lake> vec = new Vector<>();
-        for (Lake lake: Objects.requireNonNull(this.allLakes.getValue())) {
-            if (lake.name.toLowerCase().contains(query.getRawString().toLowerCase())) {
-                vec.add(lake);
-            }
-        }
-        filteredLakes.setValue(vec);
-        return filteredLakes;
+        return lakeDao.getFilteredLakes(query.getQuery());
     }
 }
