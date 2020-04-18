0c32564ab85d321c9190839f69c4d2a21b4ebdee
Luis M. Figueroa <lmanuelfigueroa@gmail.com>
2020-04-17 20:02:58 -0700

    updated UI connected to current fragments including HomepageView fragment and improve quality of images displaying in camera

--- app/src/main/java/com/example/shorebuddy/views/CameraFragment.java
+++ app/src/main/java/com/example/shorebuddy/views/CameraFragment.java
@@ -1,21 +1,30 @@
 package com.example.shorebuddy.views;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.shorebuddy.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -23,7 +32,8 @@ public class CameraFragment extends Fragment {
 
     private ImageView pictureTaken;
     private Button takePictureButton;
-    private static final int REQUEST_IMAGE_TAKEN = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
+    private String currentPhotoPath;
 
     public CameraFragment() {
     }
@@ -50,9 +60,7 @@ public class CameraFragment extends Fragment {
     }
 
     private void takePicture(View view) {
-        Intent imageTakenIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(imageTakenIntent, REQUEST_IMAGE_TAKEN);
-
+        dispatchTakePictureIntent();
 
     }
 
@@ -60,12 +68,41 @@ public class CameraFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_TAKEN && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            pictureTaken.setImageBitmap(imageBitmap);
-        }
-        }
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            File f = new File(currentPhotoPath);
+            Log.d("tag", "Absolute url of image is " + Uri.fromFile(f));
+            pictureTaken.setImageURI(Uri.fromFile(f));
 
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            }
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getActivity(), "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
 
 }
